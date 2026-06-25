ec02d665066c3eea65dc7905deacfa10c9a45f54
Leonardo Palamim Cardozo <leopalamim@gmail.com>
2026-06-25 08:33:14 -0300

    feat: google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google';
+
 import './globals.css';
 import { AppShell } from '@/modules/shell/app-shell';
 import { MagentProvider } from '@/providers/magent.provider';
@@ -35,6 +37,7 @@ const RootLayout = ({
           </MagentProvider>
         </ConnectionGate>
       </body>
+      <GoogleAnalytics gaId="G-8SNWPLW811" />
     </html>
   );
 };
